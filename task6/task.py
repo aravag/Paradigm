8dbbe6a32e6af003b20bac3cef153ab185c6ecb0
Vagan Arakelyan <vra.vagan@gmail.com>
2023-10-09 12:31:43 +0400

    Task is done

--- task6/task.py
+++ task6/task.py
@@ -13,6 +13,10 @@ def binary_search(arr, target):
     return -1
 
 def main(arr, target):
+    if sorted(arr) != arr:
+        arr.sort()
+        print(f"Массив не отсортирован. Отсортированный массив: {arr}")
+    
     result = binary_search(arr, target)
     if result != -1:
         print(f"Элемент {target} найден по индексу {result}.")
@@ -20,4 +24,4 @@ def main(arr, target):
         print(f"Элемент {target} не найден в массиве.")
 
 main([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)
-main([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
+main([10, 32, 3, 1, 8, 64, 2, 58, 1, 32], 3)
