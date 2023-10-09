c8dbc7a6451583b51d9824bfa6dbf0a04455ee6f
Vagan Arakelyan <vra.vagan@gmail.com>
2023-10-09 12:29:36 +0400

    Task is done

---
+++ task6/task.py
@@ -0,0 +1,23 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
+
+def main(arr, target):
+    result = binary_search(arr, target)
+    if result != -1:
+        print(f"Элемент {target} найден по индексу {result}.")
+    else:
+        print(f"Элемент {target} не найден в массиве.")
+
+main([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5)
+main([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8)
